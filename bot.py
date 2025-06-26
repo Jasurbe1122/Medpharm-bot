759c7ebd4301dd817467d5ac89d866616ee673da
Jasurbe1122 <asurjasur72@gmail.com>
2025-06-26 22:42:36 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,17 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from dotenv import load_dotenv
 import os
 
-# .env fayldan tokenni o‘qish
-load_dotenv()
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # ← tokenni .env fayldan oladi
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # Render'da Env Var sifatida beriladi
 
-# /start komandasi uchun javob
+# /start buyrug‘i
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom, qalaysiz? 😊")
 
-# Asosiy ishga tushirish funksiyasi
 def main():
-    app = Application.builder().token(TOKEN).build()  # ← mana bu yerda
+    app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
-# Botni ishga tushirish
 if __name__ == "__main__":
     main()
