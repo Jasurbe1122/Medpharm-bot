c9dff9f7f2687f37c5133f81e18ff89777a4fc0f
Jasurbe1122 <asurjasur72@gmail.com>
2025-06-26 22:48:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 
-# Renderda TOKEN ni Environment Variable orqali qo‘shasiz
+# Tokenni Render ichidagi "Environment Variable" orqali olamiz
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
+# /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom, qalaysiz? 😊")
 
+# Botni ishga tushirish
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
