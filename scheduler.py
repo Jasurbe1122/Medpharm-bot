f6adaa8c583a758c18821b08c46db6981399e256
Jasurbe1122 <asurjasur72@gmail.com>
2025-06-26 22:32:47 +0500

    Delete scheduler.py

--- scheduler.py
+++
@@ -1,8 +0,0 @@
-
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from datetime import datetime
-from telegram.ext import Application
-
-def setup_schedulers(app: Application):
-    scheduler = AsyncIOScheduler()
-    scheduler.start()
