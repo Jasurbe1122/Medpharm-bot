d48092b9a6b557ff805dd311028f047404f879af
Jasurbe1122 <asurjasur72@gmail.com>
2025-06-26 22:34:45 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salom, qalaysiz? 😊")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
