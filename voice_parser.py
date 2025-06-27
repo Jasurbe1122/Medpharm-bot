f8293eb19c8f9fc924fd63cf59150960ce8e3baf
Jasurbe1122 <asurjasur72@gmail.com>
2025-06-27 10:43:48 +0500

    Delete voice_parser.py

--- voice_parser.py
+++
@@ -1,8 +0,0 @@
-
-import speech_recognition as sr
-
-def convert_voice_to_text(audio_path):
-    recognizer = sr.Recognizer()
-    with sr.AudioFile(audio_path) as source:
-        audio = recognizer.record(source)
-    return recognizer.recognize_google(audio, language='uz-UZ')
