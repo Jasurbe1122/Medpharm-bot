6d8178f4a4852f5f531864f67f3a7759d7807725
Jasurbe1122 <asurjasur72@gmail.com>
2025-06-27 23:36:46 +0500

    Delete ocr_utils.py

--- ocr_utils.py
+++
@@ -1,8 +0,0 @@
-import pytesseract
-from PIL import Image
-
-def extract_text_from_image(image_path):
-    try:
-        return pytesseract.image_to_string(Image.open(image_path))
-    except:
-        return ""
